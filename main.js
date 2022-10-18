010f350fbce81d0befb49e575c6014e5e4892d72
Treasure Ajefu <treasureajefu@gmail.com>
2022-10-18 13:21:09 +0100

    done with mobile navbar

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 var toggleBtn = document.querySelector('#toggleBtn');
 var nav = document.querySelector('#nav');
-var body = document.querySelector('body');
 
-toggleBtn.addEventListener('click', function() {
-    nav.classList.toggle('hidden');
-    document.body.classList.toggle('overflow-hidden');
-    // body.classList.toggle('opacity-50');
-    
-    console.log('open navbar');
-});
+document.onclick = function(e) {
+    if (e.target.id === 'toggleBtn') {
+        nav.classList.toggle('hidden');
+        nav.classList.toggle('transition-all');
+    }
+};
+console.log(toggleBtn)
+// var nav = document.querySelector('#nav');
+// var body = document.querySelector('body');
+
+// toggleBtn.addEventListener('click', function() {
+//     nav.classList.toggle('hidden');
+//     document.body.classList.toggle('overflow-hidden');
+//     // body.classList.toggle('opacity-50');
+//     alert('holaaa')
+//     console.log('open navbar');
+// });
+console.log('main.js loaded');
