3bcc87512dfd6f74ea33d48efcf38e1587527fa1
Treasure Ajefu <treasureajefu@gmail.com>
2022-10-18 14:44:54 +0100

    finished accordion on mobile nav

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 var toggleBtn = document.querySelector('#toggleBtn');
 var nav = document.querySelector('#nav');
 
+var expandBtn = document.getElementById('expandBtn');
+var expandItem = document.querySelectorAll('#expandItem');
+var expandContent = document.getElementById('expandContent');
+
+// // driver function
+// document.onclick = function(e) {
+//     // toggle display for the navigation
+//     if (e.target.id === 'toggleBtn') {
+//         nav.classList.toggle('hidden');
+//         nav.classList.toggle('transition-all');
+//     }
+//     // toggle display for the accordion item thingy
+//     if (e.target.id === 'expandItem') {
+//         expandContent.classList.toggle('hidden');
+//         expandBtn.classList.toggle('rotate-180');
+//     } 
+// };
+
+// driver function
 document.onclick = function(e) {
+    // toggle display for the navigation
     if (e.target.id === 'toggleBtn') {
         nav.classList.toggle('hidden');
         nav.classList.toggle('transition-all');
     }
+    // toggle display for the accordion item thingy
+    for (i = 0; i < expandItem.length; i++) {
+        expandItem[i].addEventListener("click", function() {
+            var content = this.nextElementSibling;
+            expandBtn.classList.toggle('rotate-180');
+            content.classList.toggle('hidden')
+        });
+    }
 };
-console.log(toggleBtn)
 // var nav = document.querySelector('#nav');
 // var body = document.querySelector('body');
 
